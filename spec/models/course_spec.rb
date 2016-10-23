2b2ea64274ce51964cdffde36d736f360401f4cf
Pablo Bello <pablobello@Pablos-MacBook-Pro.local>
2016-10-23 08:24:04 -0300

    Add course specs

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Course, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:subtitle) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:duration) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
+    it { should validate_numericality_of(:duration).is_greater_than(0) }
+  end
+
+  describe "associations" do
+    it { should have_many :chapters }
+  end
+end
