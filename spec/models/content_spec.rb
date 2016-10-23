8c001e0f09935e88aaa800128b7d0318f7719cbb
Pablo Bello <pablobello@Pablos-MacBook-Pro.local>
2016-10-23 09:00:56 -0300

    Add content specs

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Chapter, type: :model do
+RSpec.describe Content, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_length_of(:title).is_at_most(255) }
   end
 
   describe 'associations' do
-    it { should belong_to :course }
-    it { should have_many :contents }
+    it { should belong_to :chapter }
   end
 end
